fcdda184af0b1f96ce12a91912e4d892add17cde
Stephen Sugden <grncdr@uvic.ca>
2009-12-05 16:45:56 -0800

    New multi-queue
    Maintains a dictionary of lists "atomically"

---
+++ multiqueue.py
@@ -0,0 +1,62 @@
+import threading
+from collections import deque
+
+class MultiQueue(object):
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self._lists = {}
+        self.clear()
+
+    def __len__(self):
+        self.lock.acquire()
+        length = len(self._lists)
+        self.lock.release()
+        return length
+
+    def __getitem__(self, i):
+        return self.get(i)
+
+    def keys(self):
+        return self._lists.keys()
+
+    def put(self, l, job):
+        self.lock.acquire()
+        if l not in self._lists:
+            self._lists[l] = [job]
+        else:
+            self._lists[l].append(job)
+        self.lock.release()
+
+    def get(self, l, clear=False):
+        self.lock.acquire()
+        if l not in self._lists:
+            list = []
+        else:
+            list = self._lists[l]
+            if clear:
+                del(self._lists[l])
+        self.lock.release()
+        return list
+
+    def remove(self, test):
+        self.lock.acquire()
+        for list in self._lists:
+            for item in list:
+                if test(item):
+                    list.remove(item)
+        self.lock.release()
+
+    def move(self, job, new_f):
+        for list in self.jobs:
+            for (i, cjob) in zip(range(len(list)), list):
+                if job[0:2] == cjob[0:2]:
+                    self.jobs[new_f].append(list.pop(i))
+                    return True
+        return False
+
+    def clear(self):
+        self.lock.acquire()
+        del(self._lists)
+        self._lists = {}
+        self.lock.release()
