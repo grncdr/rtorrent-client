689eff83888ee3044aa6082cb10f6f2b62640460
Stephen Sugden <grncdr@uvic.ca>
2009-12-07 00:13:25 -0800

    setup.py should now work with py2app and py2exe

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
-from distutils.core import setup
-import py2exe
+import os
 
-setup(windows=['wrtc.py'])
+APP = ['wrtc.py']
+DATA_FILES = []
+OPTIONS = {'argv_emulation': True}
 
+if os.name == 'nt':
+    from distutils.core import setup
+    import py2exe
+    setup(windows=APP)
+
+elif os.uname()[0] == 'Darwin':
+    from setuptools import setup
+    setup(
+        app=APP,
+        data_files=DATA_FILES,
+        options={'py2app': OPTIONS},
+        setup_requires=['py2app'],
+    )
