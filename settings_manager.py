f8301ecc58bf3b01ac3d07bf9dbf7294c3407d6d
Stephen Sugden <grncdr@uvic.ca>
2009-12-08 16:37:09 -0800

    Move SettingManager into own file

---
+++ settings_manager.py
@@ -0,0 +1,49 @@
+from ConfigParser import ConfigParser
+import wx
+class SettingsManager():
+    ''' Wraps a ConfigParser and shows a nice little dialog  '''
+    def __init__(self, filename, defaults={}):
+        self.cfg = ConfigParser(defaults)
+        self.filename = filename
+        self.config_path = self.get_config_path()
+        self.cfg.read(self.config_path)
+
+    def get(self, *args):
+        if not args: return None
+        if len(args) > 1: return self.cfg.get(*args)
+        return self.cfg.get("DEFAULT", args[0])
+
+    def get_config_path(self):
+        if os.name == 'nt':
+            return os.path.expanduser("~/AppData/Local/%s" % self.filename)
+        else:
+            return os.path.expanduser("~/.config/%s" % self.filename)
+
+    def show_dialog(self, evt=None):
+        self.dlg = wx.Dialog(None, title="Settings")
+        sizer = wx.FlexGridSizer(4,2,0,10)
+        sizer.SetFlexibleDirection(wx.HORIZONTAL)
+        sizer.AddGrowableCol(1)
+        self.dlg.SetSizer(sizer)
+        self.controls = [] 
+        for item in self.cfg.items("DEFAULT"):
+            k, v = item
+            control = wx.TextCtrl(self.dlg, value=v)
+            label = wx.StaticText(self.dlg, label=k.title())
+            self.controls.append((k, control,))
+            sizer.Add(label, flag=wx.EXPAND|wx.ALL, border=10)
+            sizer.Add(control, flag=wx.EXPAND|wx.ALL, border=10)
+        save_button = wx.Button(self.dlg, id=wx.ID_OK, label="Save")
+        save_button.Bind(wx.EVT_BUTTON, self.save)
+        cancel = wx.Button(self.dlg, id=wx.ID_CANCEL)
+        sizer.Add(cancel, 0, wx.ALIGN_LEFT | wx.ALL, border=10)
+        sizer.Add(save_button, 0, wx.ALIGN_RIGHT | wx.ALL, border=10)
+        self.dlg.ShowModal()
+
+    def save(self, evt):
+        for setting, control in self.controls:
+            self.cfg.set("DEFAULT", setting, str(control.GetValue()))
+        with open(self.config_path,'wb') as fh:
+            self.cfg.write(fh)
+        wx.GetApp().daemon.open(self.cfg.get("DEFAULT",'rTorrent URL'))
+        self.dlg.Close()
