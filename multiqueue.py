592a571e28fc19a18a0a6df5f984d9856ea2a86f
Stephen Sugden <grncdr@uvic.ca>
2009-12-08 16:41:59 -0800

    Switch to using with statements for locks

--- multiqueue.py
+++ multiqueue.py
@@ -1,65 +1,72 @@
+from __future__ import with_statement
 import threading
-from collections import deque
 
 class MultiQueue(object):
-
     def __init__(self):
         self.lock = threading.Lock()
         self._lists = {}
         self.clear()
 
     def __len__(self):
-        self.lock.acquire()
-        length = len(self._lists)
-        self.lock.release()
+        with self.lock:
+            #print "Locked multi-queue"
+            length = len(self._lists)
+        #print "Released multi-queue"
         return length
 
     def __getitem__(self, i):
         return self.get(i)
 
     def keys(self):
-        self.lock.acquire()
-        keys = self._lists.keys()
-        self.lock.release()
+        with self.lock:
+            #print "Locked multi-queue"
+            keys = self._lists.keys()
+        #print "Released multi-queue"
         return keys
 
-    def put(self, l, job):
-        self.lock.acquire()
-        if l not in self._lists:
-            self._lists[l] = [job]
-        else:
-            self._lists[l].append(job)
-        self.lock.release()
+    def put(self, i, job):
+        with self.lock:
+            #print "Locked multi-queue"
+            if i not in self._lists:
+                self._lists[i] = [job]
+            else:
+                self._lists[i].append(job)
 
-    def get(self, l, clear=False):
-        self.lock.acquire()
-        if l not in self._lists:
-            list = []
-        else:
-            list = self._lists[l]
-            if clear:
-                del(self._lists[l])
-        self.lock.release()
-        return list
+    def get(self, i, clear=False):
+        with self.lock:
+            #print "Locked multi-queue"
+            if i not in self._lists:
+                rv = []
+            else:
+                rv = list(self._lists[i])
+                if clear:
+                    del(self._lists[i])
+        #print "Released multi-queue"
+        return rv
 
     def remove(self, test):
-        self.lock.acquire()
-        for list in self._lists.values():
-            for item in list:
-                if test(item):
-                    list.remove(item)
-        self.lock.release()
+        with self.lock:
+            #print "Locked multi-queue"
+            for list in self._lists.values():
+                for item in list:
+                    if test(item):
+                        list.remove(item)
 
     def move(self, job, new_f):
-        for list in self.jobs:
-            for (i, cjob) in zip(range(len(list)), list):
-                if job[0:2] == cjob[0:2]:
-                    self.jobs[new_f].append(list.pop(i))
-                    return True
-        return False
+        rv = False
+        with self.lock:
+            #print "Locked multi-queue"
+            for list in self.jobs:
+                for (i, cjob) in zip(range(len(list)), list):
+                    if job[0:2] == cjob[0:2]:
+                        self.jobs[new_f].append(list.pop(i))
+                        rv = not rv
+        #print "Released multi-queue"
+        return rv
 
     def clear(self):
-        self.lock.acquire()
-        del(self._lists)
-        self._lists = {}
-        self.lock.release()
+        with self.lock:
+            #print "Locked multi-queue"
+            del(self._lists)
+            self._lists = {}
+        #print "Released multi-queue"
