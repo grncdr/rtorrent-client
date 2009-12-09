82ef0cf5c60575c2c72066024fe3dd15f255d9df
Stephen Sugden <grncdr@uvic.ca>
2009-12-08 16:40:52 -0800

    Remove SettingsManager from main script

--- wrtc.py
+++ wrtc.py
@@ -8,8 +8,8 @@ Description: This is a little app that connects to and monitors a remote rTorren
 
 from __future__ import with_statement
 import os, sys, threading, wx, time
-from ConfigParser import ConfigParser
 from wx.lib.mixins.listctrl import ListCtrlAutoWidthMixin
+from settings_manager import SettingsManager
 from xmlrpcdaemon import XMLRPCDaemon, Binary
 from multiqueue import MultiQueue
 
@@ -31,54 +31,6 @@ def make_hash(tdata):
     from bencode import bdecode, bencode
     from hashlib import sha1
     return sha1(bencode(bdecode(tdata)['info'])).hexdigest().upper()
-
-class SettingsManager():
-    ''' Wraps a ConfigParser and shows a nice little dialog  '''
-    def __init__(self, filename, defaults={}):
-        self.cfg = ConfigParser(defaults)
-        self.filename = filename
-        self.config_path = self.get_config_path()
-        self.cfg.read(self.config_path)
-
-    def get(self, *args):
-        if not args: return None
-        if len(args) > 1: return self.cfg.get(*args)
-        return self.cfg.get("DEFAULT", args[0])
-
-    def get_config_path(self):
-        if os.name == 'nt':
-            return os.path.expanduser("~/AppData/Local/%s" % self.filename)
-        else:
-            return os.path.expanduser("~/.config/%s" % self.filename)
-
-    def show_dialog(self, evt=None):
-        self.dlg = wx.Dialog(None, title="Settings")
-        sizer = wx.FlexGridSizer(4,2,0,10)
-        sizer.SetFlexibleDirection(wx.HORIZONTAL)
-        sizer.AddGrowableCol(1)
-        self.dlg.SetSizer(sizer)
-        self.controls = [] 
-        for item in self.cfg.items("DEFAULT"):
-            k, v = item
-            control = wx.TextCtrl(self.dlg, value=v)
-            label = wx.StaticText(self.dlg, label=k.title())
-            self.controls.append((k, control,))
-            sizer.Add(label, flag=wx.EXPAND|wx.ALL, border=10)
-            sizer.Add(control, flag=wx.EXPAND|wx.ALL, border=10)
-        save_button = wx.Button(self.dlg, id=wx.ID_OK, label="Save")
-        save_button.Bind(wx.EVT_BUTTON, self.save)
-        cancel = wx.Button(self.dlg, id=wx.ID_CANCEL)
-        sizer.Add(cancel, 0, wx.ALIGN_LEFT | wx.ALL, border=10)
-        sizer.Add(save_button, 0, wx.ALIGN_RIGHT | wx.ALL, border=10)
-        self.dlg.ShowModal()
-
-    def save(self, evt):
-        for setting, control in self.controls:
-            self.cfg.set("DEFAULT", setting, str(control.GetValue()))
-        with open(self.config_path,'wb') as fh:
-            self.cfg.write(fh)
-        wx.GetApp().daemon.open(self.cfg.get("DEFAULT",'rTorrent URL'))
-        self.dlg.Close()
 
 class wrtcApp(wx.App):
     def __init__(self, *args, **kwargs):
